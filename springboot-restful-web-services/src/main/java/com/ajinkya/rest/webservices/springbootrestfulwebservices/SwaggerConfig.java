21a286d6c57e58f61f0dd71efd5aceea71157e46
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-12-02 22:31:04 +1100

    Added swagger documentation

---
+++ springboot-restful-web-services/src/main/java/com/ajinkya/rest/webservices/springbootrestfulwebservices/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.ajinkya.rest.webservices.springbootrestfulwebservices;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	public static final Contact DEFAULT_CONTACT = new Contact("Ajinkya Kolhe", "", "");
+	  
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Springboot Rest API", "Springboot Rest API Description", "1.0", "urn:tos",
+	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+
+	public static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+				
+	}
+}
--- springboot-restful-web-services/src/main/java/com/ajinkya/rest/webservices/springbootrestfulwebservices/user/User.java
+++ springboot-restful-web-services/src/main/java/com/ajinkya/rest/webservices/springbootrestfulwebservices/user/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about user.")
 public class User {
 
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atlease 2 chracters")
+	@ApiModelProperty(notes="Name should have atlease 2 chracters")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes="Birth date should be in the past")
 	private Date birthDate;
 
 	protected User() {
