27a183993d6dfdb9cf7af9e9837c4d5e828c3cd2
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-12-03 14:10:03 +1100

    Versioning using basic approach with URI

---
+++ springboot-restful-web-services/src/main/java/com/ajinkya/rest/webservices/springbootrestfulwebservices/versioning/Name.java
@@ -0,0 +1,36 @@
+package com.ajinkya.rest.webservices.springbootrestfulwebservices.versioning;
+
+public class Name {
+
+	private String firstName;
+	
+	private String lastName;
+
+	public Name() {
+		
+	}
+	
+	public Name(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
