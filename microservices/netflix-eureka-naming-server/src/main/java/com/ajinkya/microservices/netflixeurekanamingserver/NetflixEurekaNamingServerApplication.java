13c183d79ae86ea8b10d246701c54b2ad54fb04c
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-12-12 20:58:54 +1100

    Added eureka naming server

---
+++ microservices/netflix-eureka-naming-server/src/main/java/com/ajinkya/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,12 @@
+package com.ajinkya.microservices.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+}
