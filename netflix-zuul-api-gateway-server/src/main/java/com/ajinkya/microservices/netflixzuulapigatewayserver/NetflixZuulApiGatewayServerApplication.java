8140f0c486214f027a47fcd9ded7a14272f4b3ac
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-12-16 04:46:43 +1100

    Setting up zuul api gateway

--- netflix-zuul-api-gateway-server/src/main/java/com/ajinkya/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ netflix-zuul-api-gateway-server/src/main/java/com/ajinkya/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -2,7 +2,11 @@ package com.ajinkya.microservices.netflixzuulapigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
+@EnableZuulServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NetflixZuulApiGatewayServerApplication {
 
